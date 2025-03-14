aa232a7c28da019bffce067a224cdf758db1f00c
meaniebeanie22 <73771297+meaniebeanie22@users.noreply.github.com>
2025-03-14 22:30:57 +1100

    Add checking to ensure the date actually makes sense

--- 20250314.py
+++ 20250314.py
@@ -1,22 +1,25 @@
 MONTH_DAY = {
-    "january": 0,
-    "february": 31,
-    "march": 59,
-    "april": 90,
-    "may": 120,
-    "june": 151,
-    "july": 181,
-    "august": 212,
-    "september": 243,
-    "october": 273,
-    "november": 304,
-    "december": 334,
+    "january": (0, 31),
+    "february": (31, 28),
+    "march": (59, 31),
+    "april": (90, 30),
+    "may": (120, 31),
+    "june": (151, 30),
+    "july": (181, 31),
+    "august": (212, 31),
+    "september": (243, 30),
+    "october": (273, 31),
+    "november": (304, 30),
+    "december": (334, 31),
 }
 month, day = input("Month: ").lower(), int(input("Day: "))
+if not 0 < day <= MONTH_DAY[month][1]:
+    print("Invalid")
+    exit()
 try:
-    day_of_year = MONTH_DAY[month] + day
+    day_of_year = MONTH_DAY[month][0] + day
 except KeyError:
-    print("Invalid month entered")
+    print("Invalid")
 
 if day_of_year >= 335 or day_of_year < 79:
     print("Summer")
